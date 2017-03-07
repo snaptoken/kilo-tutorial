fea4fad416d10061076bcabdd0b7822552ee649b
Jeremy Ruten <jeremy.ruten@gmail.com>
2017-03-06 23:20:03 -0600

    steps 34-41

---
+++ leg/36/kilo.c
@@ -0,0 +1,312 @@
+/* # A line viewer
+ */
+
+/*** includes ***/
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** defines ***/
+
+#define _DEFAULT_SOURCE
+#define _GNU_SOURCE
+
+#define KILO_VERSION "0.0.1"
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+enum {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
+
+/*** data ***/
+
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
+struct editorConfig {
+  int cx, cy;
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+  int numrows;
+  erow row;
+};
+
+struct editorConfig E;
+
+/*** terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void enableRawMode() {
+  struct termios raw;
+
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+
+  raw = E.orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+int editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) == 0);
+  if (nread == -1) die("read");
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, seq, 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, seq + 1, 1) != 1) return '\x1b';
+
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+      }
+    }
+
+    return '\x1b';
+  } else {
+    return c;
+  }
+}
+
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, buf + i, 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(buf + 2, "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+/*** editor ***/
+
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    if (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen);
+    memcpy(E.row.chars, line, linelen);
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
+}
+
+/*** output ***/
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(new + ab->len, s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
+void editorRefreshScreen() {
+  struct abuf ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
+
+  int row;
+  for (row = 0; row < E.screenrows; row++) {
+    if (row >= E.numrows) {
+      if (E.numrows == 0 && row == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Kilo editor -- version %s", KILO_VERSION);
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(&ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(&ab, " ", 1);
+        abAppend(&ab, welcome, welcomelen);
+      } else {
+        abAppend(&ab, "~", 1);
+      }
+    } else {
+      int len = E.row.size;
+      if (len > 0) {
+        if (len > E.screencols) len = E.screencols;
+        abAppend(&ab, E.row.chars, len);
+      }
+    }
+
+    abAppend(&ab, "\x1b[K", 3);
+    if (row < E.screenrows - 1) {
+      abAppend(&ab, "\r\n", 2);
+    }
+  }
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
+}
+
+/*** input ***/
+
+void editorMoveCursor(int key) {
+  switch (key) {
+    case ARROW_LEFT:
+      if (E.cx != 0) {
+        E.cx--;
+      }
+      break;
+    case ARROW_RIGHT:
+      if (E.cx != E.screencols - 1) {
+        E.cx++;
+      }
+      break;
+    case ARROW_UP:
+      if (E.cy != 0) {
+        E.cy--;
+      }
+      break;
+    case ARROW_DOWN:
+      if (E.cy != E.screenrows - 1) {
+        E.cy++;
+      }
+      break;
+  }
+}
+
+void editorProcessKeypress() {
+  int c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+      editorMoveCursor(c);
+      break;
+  }
+}
+
+/*** init ***/
+
+void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+  E.numrows = 0;
+
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
+int main(int argc, char *argv[]) {
+  enableRawMode();
+  initEditor();
+  if (argc == 2) {
+    editorOpen(argv[1]);
+  }
+
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  }
+
+  return 0;
+}
+
+/* Explain:
+ *   - `fopen()`, `fclose()`, `FILE` come from `<stdio.h>`
+ *   - `getline()` comes from `<stdio.h>` (but may require defining
+ *   - `size_t`, `ssize_t` come from `<sys/types.h>`
+ *     `_DEFAULT_SOURCE` and/or `_GNU_SOURCE`
+ *   - `typedef struct`
+ *   - We'll store lines without newlines or even NUL terminators at the end,
+ *     storing the length of each line instead
+ *   - Replacing `\x1b[2J` with `\x1b[K`
+ *   - Display welcome message only if new file
+ *   - `argc` and `argv`
+ */
+
