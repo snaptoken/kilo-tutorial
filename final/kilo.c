7c2ee392f5b4a7ac69e5bf9da36825f2b65aedc6
Jeremy Ruten <jeremy.ruten@gmail.com>
2017-03-10 13:41:54 -0600

    split editorRefreshScreen() into 3 more functions

--- final/kilo.c
+++ final/kilo.c
@@ -738,14 +738,7 @@ void editorScroll() {
   }
 }
 
-void editorRefreshScreen() {
-  editorScroll();
-
-  struct abuf ab = ABUF_INIT;
-
-  abAppend(&ab, "\x1b[?25l", 6);
-  abAppend(&ab, "\x1b[H", 3);
-
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     int filerow = E.rowoff + y;
@@ -757,13 +750,13 @@ void editorRefreshScreen() {
           "Kilo editor -- version %s", KILO_VERSION);
         int padding = (E.screencols - welcomelen) / 2;
         if (padding) {
-          abAppend(&ab, "~", 1);
+          abAppend(ab, "~", 1);
           padding--;
         }
-        while (padding--) abAppend(&ab, " ", 1);
-        abAppend(&ab, welcome, welcomelen);
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
       } else {
-        abAppend(&ab, "~", 1);
+        abAppend(ab, "~", 1);
       }
     } else {
       int len = E.row[filerow].rsize - E.coloff;
@@ -776,36 +769,38 @@ void editorRefreshScreen() {
         for (j = 0; j < len; j++) {
           if (!isprint(c[j])) {
             char sym = (c[j] >= 0 && c[j] <= 26) ? '@' + c[j] : '?';
-            abAppend(&ab, "\x1b[7m", 4);
-            abAppend(&ab, &sym, 1);
-            abAppend(&ab, "\x1b[0m", 4);
+            abAppend(ab, "\x1b[7m", 4);
+            abAppend(ab, &sym, 1);
+            abAppend(ab, "\x1b[0m", 4);
             current_color = -1;
           } else if (hl[j] == HL_NORMAL) {
             if (current_color != -1) {
-              abAppend(&ab, "\x1b[39m", 5);
+              abAppend(ab, "\x1b[39m", 5);
               current_color = -1;
             }
-            abAppend(&ab, &c[j], 1);
+            abAppend(ab, &c[j], 1);
           } else {
             int color = editorSyntaxToColor(hl[j]);
             if (color != current_color) {
               current_color = color;
               char buf[16];
               int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
-              abAppend(&ab, buf, clen);
+              abAppend(ab, buf, clen);
             }
-            abAppend(&ab, &c[j], 1);
+            abAppend(ab, &c[j], 1);
           }
         }
-        abAppend(&ab, "\x1b[39m", 5);
+        abAppend(ab, "\x1b[39m", 5);
       }
     }
 
-    abAppend(&ab, "\x1b[K", 3);
-    abAppend(&ab, "\r\n", 2);
+    abAppend(ab, "\x1b[K", 3);
+    abAppend(ab, "\r\n", 2);
   }
+}
 
-  abAppend(&ab, "\x1b[7m", 4);
+void editorDrawStatusBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
   int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
     E.filename ? E.filename : "[No Name]", E.numrows,
@@ -813,24 +808,39 @@ void editorRefreshScreen() {
   int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d",
     E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
   if (len > E.screencols) len = E.screencols;
-  abAppend(&ab, status, len);
+  abAppend(ab, status, len);
   while (len < E.screencols) {
     if (E.screencols - len == rlen) {
-      abAppend(&ab, rstatus, rlen);
+      abAppend(ab, rstatus, rlen);
       break;
     } else {
-      abAppend(&ab, " ", 1);
+      abAppend(ab, " ", 1);
       len++;
     }
   }
-  abAppend(&ab, "\x1b[0m", 4);
-  abAppend(&ab, "\r\n", 2);
+  abAppend(ab, "\x1b[0m", 4);
+  abAppend(ab, "\r\n", 2);
+}
 
-  abAppend(&ab, "\x1b[K", 3);
+void editorDrawMessageBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[K", 3);
   int msglen = strlen(E.statusmsg);
   if (msglen > E.screencols) msglen = E.screencols;
   if (msglen && time(NULL) - E.statusmsg_time < 5)
-    abAppend(&ab, E.statusmsg, msglen);
+    abAppend(ab, E.statusmsg, msglen);
+}
+
+void editorRefreshScreen() {
+  editorScroll();
+
+  struct abuf ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
+  editorDrawMessageBar(&ab);
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
