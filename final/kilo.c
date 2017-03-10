982c59100a036a7f24ffcaefa3472d89326a97f9
Jeremy Ruten <jeremy.ruten@gmail.com>
2017-03-10 13:47:37 -0600

    make DEL key delete forward

--- final/kilo.c
+++ final/kilo.c
@@ -980,6 +980,7 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
+      if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
       editorDelChar();
       break;
 
